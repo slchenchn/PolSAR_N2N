2ea9d2f19495cf2c2335796f28f9df94269eb184
ChengShuaiLin <1106982578@qq.com>
2021-05-30 14:20:14 +0800

    nono

--- multi_trails_amazon.sh
+++ multi_trails_amazon.sh
@@ -4,11 +4,12 @@ cd /home/csl/code/PolSAR_N2N
 # python train.py --data.log False --train.optimizer.lr 1e-6
 # python train.py --data.log False --train.optimizer.lr 1e-7
 
-python train.py --data.log True --train.optimizer.weight_decay 1e-7
-python train.py --data.log True --train.optimizer.weight_decay 1e-6
-python train.py --data.log True --train.optimizer.weight_decay 1e-5
-python train.py --data.log True --train.optimizer.weight_decay 1e-4
+# python train.py --data.log True --train.optimizer.weight_decay 1e-7
+# python train.py --data.log True --train.optimizer.weight_decay 1e-6
+# python train.py --data.log True --train.optimizer.weight_decay 1e-5
+# python train.py --data.log True --train.optimizer.weight_decay 1e-4
 
+python train.py --data.log True --data.ENL 10 --train.epoch 150
 
 # python train.py --train.optimizer.lr 1e-5
 # python train.py --train.optimizer.lr 1e-7
