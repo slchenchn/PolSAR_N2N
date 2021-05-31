7a0213d6451572e1fff5537980fe602943172e23
ChengShuaiLin <1106982578@qq.com>
2021-05-31 15:28:05 +0800

    none

--- multi_trails_amazon.sh
+++ multi_trails_amazon.sh
@@ -9,7 +9,9 @@ cd /home/csl/code/PolSAR_N2N
 # python train.py --data.log True --train.optimizer.weight_decay 1e-5
 # python train.py --data.log True --train.optimizer.weight_decay 1e-4
 
-python train.py --data.log True --data.ENL 10 --train.epoch 150
+# python train.py --data.log True --data.ENL 10 --train.epoch 150
+
+python train.py --data.log True --data.ENL 10 --config_file configs/hoekman_unetpp_simulate_step.yml
 
 # python train.py --train.optimizer.lr 1e-5
 # python train.py --train.optimizer.lr 1e-7
