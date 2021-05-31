4ced59ee65d740916ac6de4c5f4f08510e45e132
ChengShuaiLin <1106982578@qq.com>
2021-05-31 13:54:25 +0800

    none

---
+++ ptsemseg/loader/polsar_simulate_n2n.py
@@ -0,0 +1,169 @@
+'''
+Author: Shuailin Chen
+Created Date: 2021-03-05
+Last Modified: 2021-05-28
+	content: 
+'''
+import shutil
+import os
+import os.path as osp
+import sys
+from typing import Union
+import re
+from random import shuffle
+from glob import glob
+
+import torch
+from torch import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils import data
+from torchvision import transforms
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt 
+from scipy import special
+
+from mylib import polSAR_utils as psr
+from mylib import simulate
+from mylib import labelme_utils as lbm
+from mylib import file_utils as fu
+from mylib import types
+from mylib import mathlib
+from mylib import my_torch_tools as mt
+# import ptsemseg.augmentations.augmentations
+
+_TMP_PATH = './tmp'
+
+class PolSARSimulate(data.Dataset):
+    ''' PolSAR Neighbor2Neighbor dataloaders for simulated data
+    
+    Args:
+        file_root (str): file root path
+        split_root (str): split file root path
+        split (str): "train" or "val" or "test". Default: "train"
+        augments (Compose): compose of augmentations. Default: None
+        data_format (str): "Hoekman" or "complex_vector_6" or 
+            "complex_vector_9" or 's2'. Default: "Hoekman"
+        norm (bool): whether to read normed or unnormed data. Default: False
+        log (bool): whether to log transform the data value, only valid for hoekman decomposition. Default: True
+        ENL (int): equivalent numober of looks. Default: 1
+
+    '''
+    
+    def __init__(self, 
+                file_root,        
+                split_root,
+                split = "train",
+                augments=None,
+                data_format = 'Hoekman',
+                norm=False,
+                log = True,
+                ENL = 1,
+                logger = None,
+                ):
+        super().__init__()
+        self.file_root = file_root
+        self.augments = augments
+        self.data_format = data_format
+        self.norm = norm
+        self.log = log
+        self.ENL = ENL
+
+        # compensate for the mean value of log tranformed intensites
+        if log:
+            self.log_compensation = np.log(ENL) - special.digamma(ENL)
+
+        # read all files' path
+        self.files_path = glob(osp.join(file_root, r'*/*.jpg'))
+        
+        info = f'split: {split}\n\tfile root: {file_root}\n\tdata format: {data_format}\n\tnorm: {norm}\n\tlen: {self.__len__()}\n\tENL: {ENL}\n\tlog transform:{log}'
+
+        if logger:
+            logger.info(info)
+        else:
+            print(info)
+
+    def __len__(self):
+        return len(self.files_path)
+
+    def __getitem__(self, index):
+
+        # read image and simulate Wishart noise
+        img = cv2.imread(self.files_path[index]).astype(np.float32)
+        # print(f'file path {self.files_path[index]}')
+        img = img[:256, :256, :]    # ensure can be divede by 32
+        img[img<1] += 1         # avoid zero value
+
+
+        if self.augments:
+            img = self.augments(torch.from_numpy(img))
+
+        if isinstance(img, torch.Tensor):
+            img = img.numpy()
+        img, noise = simulate.generate_Wishart_noise_from_img(img, self.ENL)        
+        
+
+        pauli_img = psr.rgb_by_c3(img, type='sinclair')
+        pauli_noise = psr.rgb_by_c3(noise, type='sinclair')
+        cv2.imwrite(osp.join(_TMP_PATH, 'pauli_img.jpg'), cv2.cvtColor((255*pauli_img).astype(np.uint8), cv2.COLOR_RGB2BGR))
+        cv2.imwrite(osp.join(_TMP_PATH, 'pauli_noise.jpg'), cv2.cvtColor((255*pauli_noise).astype(np.uint8), cv2.COLOR_RGB2BGR))
+
+
+        # hoekman decomposition
+        img = psr.Hokeman_decomposition(img)
+        noise = psr.Hokeman_decomposition(noise)
+
+        # log transform
+        if self.log:
+            img = np.log(img) + self.log_compensation
+            noise = np.log(noise) + self.log_compensation
+        
+        # to pytorch
+        img = torch.from_numpy(img)
+        noise = torch.from_numpy(noise)
+
+        return img, noise, self.files_path[index]
+
+    def Hoekman_recover_to_C3(self, h):
+        ''' Recover hoekman data to C3 data
+
+        Args:
+            h (Tensor): in shape of [batch, channel, height, width]
+        
+        Returns
+            C3 (ndarray): C3 matrix, in shape of [batch, channel, height, width]
+        '''
+
+        # to numpy
+        if 'cpu' != h.device:
+            h = h.cpu()
+        if isinstance(h, Tensor):
+            h = h.numpy()
+
+        # inverse log transform
+        if self.log:
+            h = np.exp(h - self.log_compensation)
+
+        # invere hoekman decomposition
+        C3 = np.empty(h.shape)
+        for ii in range(h.shape[0]):
+            C3[ii, ...] = psr.inverse_Hokeman_decomposition(h[ii, ...])
+
+        return C3
+
+
+if __name__=='__main__':
+        
+    h = 100
+    img = np.arange(h**2).reshape(h, h, 1)
+    img = np.tile(img, (1, 1, 3))
+    img = np.transpose(img, (2, 0, 1))
+
+    print(f'before:\n{img}')
+    print('\n')
+    
+    print('done')
+
+
+
---
+++ train_n2n.py
@@ -0,0 +1,346 @@
+'''
+Author: Shuailin Chen
+Created Date: 2020-11-27
+Last Modified: 2021-05-30
+	content: 
+'''
+''' 
+'''
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1, 2, 3"
+# cuda_idx = [0]
+import os.path as osp
+import sys
+import time
+import shutil
+import random
+import argparse
+
+import yaml
+import torch
+import datetime
+import numpy as np
+import glob
+import natsort
+import re
+import logging
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.models as models
+from torch.utils import data 
+from torchlars import LARS
+import cv2
+import sewar
+import piq
+
+from ptsemseg.models import get_model
+from ptsemseg.loss import get_loss_function
+from ptsemseg.loader import get_loader 
+from ptsemseg.utils import get_logger
+from ptsemseg.metrics import runningScore, averageMeter
+from ptsemseg.augmentations import get_composed_augmentations
+from ptsemseg.schedulers import get_scheduler
+from ptsemseg.optimizers import get_optimizer
+from ptsemseg.loader import rand_pool
+
+
+from mylib import nestargs
+from mylib import types
+from mylib import file_utils as fu
+from mylib import my_torch_tools as tt
+from mylib.torchsummary import summary
+from utils import args
+from utils import utils
+
+
+__TMP_DIR = r'./tmp'
+
+
+def train(cfg, writer, logger):
+
+    # Setup Augmentations
+    augmentations = cfg.train.augment
+    logger.info(f'using augments: {augmentations}')
+    data_aug = get_composed_augmentations(augmentations)
+
+    # Setup Dataloader
+    data_loader = get_loader(cfg.data.dataloader)
+    data_path = cfg.data.path
+    logger.info("data path: {}".format(data_path))
+
+    t_loader = data_loader(
+        data_path,
+        data_format = cfg.data.format,
+        norm = cfg.data.norm,
+        split='train',
+        split_root = cfg.data.split,
+        augments=data_aug,
+        logger=logger,
+        log = cfg.data.log,
+        ENL = cfg.data.ENL,
+        )
+
+    v_loader = data_loader(
+        data_path,
+        data_format = cfg.data.format,
+        split='val',
+        log = cfg.data.log,
+        split_root = cfg.data.split,
+        logger=logger,
+        ENL = cfg.data.ENL,
+        )
+
+    train_data_len = len(t_loader)
+    logger.info(f'num of train samples: {train_data_len} \nnum of val samples: {len(v_loader)}')
+
+    batch_size = cfg.train.batch_size
+    epoch = cfg.train.epoch
+    train_iter = int(np.ceil(train_data_len / batch_size) * epoch)
+    logger.info(f'total train iter: {train_iter}')
+
+    trainloader = data.DataLoader(t_loader,
+                                  batch_size=batch_size, 
+                                  num_workers=cfg.train.n_workers, 
+                                  shuffle=True,
+                                  persistent_workers=True,
+                                  drop_last=True)
+
+    valloader = data.DataLoader(v_loader, 
+                                batch_size=cfg.test.batch_size, 
+                                # persis
+                                num_workers=cfg.train.n_workers,
+                                )
+
+    # Setup Model
+    device = f'cuda:{cfg.train.gpu[0]}'
+    model = get_model(cfg.model).to(device)
+    input_size = (cfg.model.in_channels, 512, 512)
+    logger.info(f"Using Model: {cfg.model.arch}")
+    # logger.info(f'model summary: {summary(model, input_size=(input_size, input_size), is_complex=False)}')
+    model = torch.nn.DataParallel(model, device_ids=cfg.gpu)      #自动多卡运行，这个好用
+    
+    # Setup optimizer, lr_scheduler and loss function
+    optimizer_cls = get_optimizer(cfg)
+    optimizer_params = {k:v for k, v in vars(cfg.train.optimizer).items()
+                        if k not in ('name', 'wrap')}
+    optimizer = optimizer_cls(model.parameters(), **optimizer_params)
+    logger.info("Using optimizer {}".format(optimizer))
+    if hasattr(cfg.train.optimizer, 'wrap') and  cfg.train.optimizer.wrap=='lars':
+        optimizer = LARS(optimizer=optimizer)
+        logger.info(f'warp optimizer with {cfg.train.optimizer.wrap}')
+    scheduler = get_scheduler(optimizer, cfg.train.lr)
+    # loss_fn = get_loss_function(cfg)
+    # logger.info(f"Using loss ,{str(cfg.train.loss)}")
+
+    # load checkpoints
+    val_cls_1_acc = 0
+    best_cls_1_acc_now = 0
+    best_cls_1_acc_iter_now = 0
+    val_macro_OA = 0
+    best_macro_OA_now = 0
+    best_macro_OA_iter_now = 0
+    start_iter = 0
+    if cfg.train.resume is not None:
+        if os.path.isfile(cfg.train.resume):
+            logger.info(
+                "Loading model and optimizer from checkpoint '{}'".format(cfg.train.resume)
+            )
+
+            # load model state
+            checkpoint = torch.load(cfg.train.resume)
+            model.load_state_dict(checkpoint["model_state"])
+            optimizer.load_state_dict(checkpoint["optimizer_state"])
+            scheduler.load_state_dict(checkpoint["scheduler_state"])
+            # best_cls_1_acc_now = checkpoint["best_cls_1_acc_now"]
+            # best_cls_1_acc_iter_now = checkpoint["best_cls_1_acc_iter_now"]
+
+            start_iter = checkpoint["epoch"]
+            logger.info(
+                "Loaded checkpoint '{}' (iter {})".format(
+                    cfg.train.resume, checkpoint["epoch"]
+                )
+            )
+
+            # copy tensorboard files
+            resume_src_dir = osp.split(cfg.train.resume)[0]
+            # shutil.copytree(resume_src_dir, writer.get_logdir())
+            for file in os.listdir(resume_src_dir):
+                if not ('.log' in file or '.yml' in file or '_last_model' in file):
+                # if 'events.out.tfevents' in file:
+                    resume_dst_dir = writer.get_logdir()
+                    fu.copy(osp.join(resume_src_dir, file), resume_dst_dir, )
+
+        else:
+            logger.info("No checkpoint found at '{}'".format(cfg.train.resume))
+
+
+    data_range = 255
+    if cfg.data.log:
+        data_range = np.log(data_range)
+
+    # Setup Metrics
+    running_metrics_val = runningScore(2)
+    runing_metrics_train = runningScore(2)
+    val_loss_meter = averageMeter()
+    train_time_meter = averageMeter()
+    train_loss_meter = averageMeter()
+    val_psnr_meter = averageMeter()
+    val_ssim_meter = averageMeter()
+
+    # train
+    it = start_iter
+    train_start_time = time.time() 
+    train_val_start_time = time.time()
+    model.train()   
+    while it < train_iter:
+        for clean, noisy, _ in trainloader:
+            it += 1   
+
+            noisy = noisy.to(device, dtype=torch.float32)
+            mask1, mask2 = rand_pool.generate_mask_pair(noisy)
+            noisy_sub1 = rand_pool.generate_subimages(noisy, mask1)
+            noisy_sub2 = rand_pool.generate_subimages(noisy, mask2)
+
+            # preparing for the regularization term
+            with torch.no_grad():
+                noisy_denoised = model(noisy)
+            noisy_sub1_denoised = rand_pool.generate_subimages(noisy_denoised, mask1)
+            noisy_sub2_denoised = rand_pool.generate_subimages(noisy_denoised, mask2)
+            # print(rand_pool.operation_seed_counter)
+
+            # calculating the loss 
+            noisy_output = model(noisy_sub1)
+            noisy_target = noisy_sub2
+            if cfg.train.loss.gamma.const:
+                gamma = cfg.train.loss.gamma.base
+            else:
+                gamma = it / train_iter * cfg.train.loss.gamma.base
+
+            diff = noisy_output - noisy_target
+            exp_diff = noisy_sub1_denoised - noisy_sub2_denoised
+            loss1 = torch.mean(diff**2)
+            loss2 = gamma * torch.mean((diff - exp_diff)**2)
+            loss_all = loss1 + loss2
+            loss_all.backward()
+
+            # In PyTorch 1.1.0 and later, you should call `optimizer.step()` before `lr_scheduler.step()`
+            optimizer.step()
+            scheduler.step()
+            
+            # record the loss of the minibatch
+            train_loss_meter.update(loss_all)
+            train_time_meter.update(time.time() - train_start_time)
+            writer.add_scalar('lr', optimizer.param_groups[0]['lr'], it)
+
+            if it % 1000==0:
+                writer.add_histogram('hist/pred', noisy_denoised, it)
+                writer.add_histogram('hist/noisy', noisy, it)
+
+                if cfg.data.simulate:
+                    writer.add_histogram('hist/clean', clean, it)
+
+            if cfg.data.simulate:
+                pass
+
+            # print interval
+            if it % cfg.train.print_interval == 0:
+                terminal_info = f"Iter [{it:d}/{train_iter:d}]  \
+                                train Loss: {train_loss_meter.avg:.4f}  \
+                                Time/Image: {train_time_meter.avg / cfg.train.batch_size:.4f}"  
+
+                logger.info(terminal_info)
+                writer.add_scalar('loss/train_loss', train_loss_meter.avg, it)
+                
+                if cfg.data.simulate:
+                    pass
+
+                runing_metrics_train.reset()
+                train_time_meter.reset()
+                train_loss_meter.reset()
+
+            # val interval
+            if it % cfg.train.val_interval == 0 or \
+               it == train_iter:
+                val_start_time = time.time()
+                model.eval()            
+                with torch.no_grad():   
+                    for clean, noisy, _ in valloader:      
+                        noisy = noisy.to(device, dtype=torch.float32)
+                        noisy_denoised = model(noisy)
+                        
+                        if cfg.data.simulate:
+                            clean = clean.to(device, dtype=torch.float32)
+                            psnr = piq.psnr(clean, noisy_denoised, data_range=data_range)
+                            ssim = piq.ssim(clean, noisy_denoised, data_range=data_range)
+                            val_psnr_meter.update(psnr)
+                            val_ssim_meter.update(ssim)
+                        
+                        val_loss = torch.mean((noisy_denoised-noisy)**2)
+                        val_loss_meter.update(val_loss)
+
+                writer.add_scalar('loss/val_loss', val_loss_meter.avg, it)
+                logger.info(f"Iter [{it}/{train_iter}], val Loss: {val_loss_meter.avg:.4f} Time/Image: {(time.time()-val_start_time)/len(v_loader):.4f}")
+                val_loss_meter.reset()
+                running_metrics_val.reset()
+                
+                if cfg.data.simulate:
+                    writer.add_scalars('metrics/val', {'psnr': val_psnr_meter.avg, 'ssim': val_ssim_meter.avg}, it)
+                    logger.info(f'psnr: {val_psnr_meter.avg},\tssim: {val_ssim_meter.avg}')
+                    val_psnr_meter.reset()
+                    val_ssim_meter.reset()
+
+
+
+                train_val_time = time.time() - train_val_start_time
+                remain_time = train_val_time * (train_iter-it) / it
+                m, s = divmod(remain_time, 60)
+                h, m = divmod(m, 60)
+                if s != 0:
+                    train_time = "Remain train time = %d hours %d minutes %d seconds \n" % (h, m, s)
+                else:
+                    train_time = "Remain train time : train completed.\n"
+                logger.info(train_time)
+                model.train()
+
+            # save model
+            if it % (train_iter/cfg.train.epoch*10) == 0:
+                ep = int(it / (train_iter/cfg.train.epoch))
+                state = {
+                    "epoch": it,
+                    "model_state": model.state_dict(),
+                    "optimizer_state": optimizer.state_dict(),
+                    "scheduler_state": scheduler.state_dict(),
+                }
+                save_path = osp.join(writer.file_writer.get_logdir(), f"{ep}.pkl")
+                torch.save(state, save_path)
+                logger.info(f'saved model state dict at {save_path}')
+
+            train_start_time = time.time() 
+
+
+if __name__ == "__main__":
+    cfg = args.get_argparser('configs/hoekman_unetpp_simulate.yml')
+    
+    # choose deterministic algorithms, and disable benchmark for variable size input
+    utils.set_random_seed(0)
+    
+    # generate work dir
+    run_id = osp.join(r'runs', cfg.model.arch + '_' + cfg.train.loss.name + '_' + cfg.train.optimizer.name+ '_' + str(cfg.train.epoch))
+    run_id = utils.get_work_dir(run_id)
+    writer = SummaryWriter(log_dir=run_id)
+    # config_fig = types.dict2fig(cfg.to_flatten_dict())
+    # writer.add_figure('config', config_fig, close=True)
+    # writer.flush()
+
+    # logger
+    logger = get_logger(run_id)
+
+    # print('-'*100)
+    logger.info(f'RUNDIR: {run_id}')
+    logger.info(f'using config file: {cfg.config_file}')
+    shutil.copy(cfg.config_file, run_id)
+
+    train(cfg, writer, logger)
+    logger.info(f'RUNDIR:{run_id}')
